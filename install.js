7f6295e60963a82654584b5f603c3b1c5de671aa
jiangxuan <jiangxuan@dxy.cn>
2019-11-25 16:45:30 +0800

    download zip file

--- install.js
+++ install.js
@@ -1,3 +1,29 @@
-const { execSync } = require('child_process')
+const path = require('path')
+const os = require('os')
 
-execSync('npm run build')
+// 执行文件的存放目录
+const EXEC_FILE_STORE_PATH = path.resolve(__dirname, './.exec')
+// 下载文件的 host
+const DOWNLOAD_HOST = 'https://assets.tuchuang.space'
+
+let downloadUrl
+
+if (os.platform() === 'darwin') {
+  downloadUrl = `${DOWNLOAD_HOST}/copy-logo-to-clipboard-1.0.0-mac.zip`
+} else if (os.platform() === 'win32') {
+  downloadUrl = `${DOWNLOAD_HOST}/copy-logo-to-clipboard-1.0.0-win32.zip`
+} else {
+  throw Error(`不支持的平台, ${os.platform()}`)
+}
+
+const download = require('download')
+
+const downloadExecFile = async () => {
+  const dulpex = download(downloadUrl, EXEC_FILE_STORE_PATH)
+
+  dulpex.on('downloadProgress', ({ percent }) => {
+    console.log(`下载进度: ${percent * 100}%`)
+  })
+}
+
+downloadExecFile()
