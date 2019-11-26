47b3679fdf945ce0e7631aa36e6dcef77c02efd8
jiangxuan <jiangxuan@dxy.cn>
2019-11-26 12:33:24 +0800

    update version

--- install.js
+++ install.js
@@ -9,9 +9,9 @@ const DOWNLOAD_HOST = 'https://assets.tuchuang.space'
 let downloadUrl
 
 if (os.platform() === 'darwin') {
-  downloadUrl = `${DOWNLOAD_HOST}/copy-logo-to-clipboard-1.0.0-mac.zip`
+  downloadUrl = `${DOWNLOAD_HOST}/copy-logo-to-clipboard-0.1.0-mac.zip`
 } else if (os.platform() === 'win32') {
-  downloadUrl = `${DOWNLOAD_HOST}/copy-logo-to-clipboard-1.0.0-win32.zip`
+  downloadUrl = `${DOWNLOAD_HOST}/copy-logo-to-clipboard-0.1.0-win32.zip`
 } else {
   throw Error(`不支持的平台, ${os.platform()}`)
 }
