a6b47108b80eb9a32954b0e217914a33e6754d36
jiangxuan <jiangxuan@dxy.cn>
2019-11-26 16:06:27 +0800

    fix path bug

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const { platform } = require('os')
+const path = require('path')
 const { exec } = require('child_process')
 const { promisify } = require('util')
 
 const promisifyExec = promisify(exec)
+
+const execPath = path.resolve(__dirname, './.exec')
 
 module.exports = {
   logoMd5Hash: '3c7185dd08b19791c4141f089616952f',
   copyLogoToClip: async () => {
     if (platform() === 'win32') {
       return promisifyExec(
-        'cd .\\.exec\\win-unpacked && .\\copy-logo-to-clipboard.exe'
+        `cd ${execPath} && cd .\\win-unpacked && .\\copy-logo-to-clipboard.exe`
       )
     } else if (platform() === 'darwin') {
       return promisifyExec(
-        'cd ./.exec/mac/copy-logo-to-clipboard.app/Contents/MacOS && ./copy-logo-to-clipboard'
+        `cd ${execPath} && cd ./mac/copy-logo-to-clipboard.app/Contents/MacOS && ./copy-logo-to-clipboard`
       )
     } else {
       throw new Error('only support win32, darwin')
